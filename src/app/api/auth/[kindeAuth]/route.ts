ca464c8cfee379206a932f301d09ca0f09d6151b
Mauricio Girardi <maurigirarde@yahoo.com.br>
2025-06-05 21:38:28 +0100

    fix: router

--- src/app/api/auth/[kindeAuth]/route.ts
+++ src/app/api/auth/[kindeAuth]/route.ts
@@ -1,8 +1,2 @@
 import { handleAuth } from '@kinde-oss/kinde-auth-nextjs/server'
-import type { NextRequest } from 'next/server'
-
-// biome-ignore lint/suspicious/noExplicitAny: <explanation>
-export async function GET(request: NextRequest, { params }: any) {
-  const endpoint = params.kindeAuth
-  return handleAuth(request, endpoint)
-}
+export const GET = handleAuth()
